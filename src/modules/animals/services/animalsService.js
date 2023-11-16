e729b23dedff5b4042f24ee06d91400f647ca8a7
Sokilskill <vprovorotov@gmail.com>
2023-11-16 09:30:19 +0200

    animalService

---
+++ src/modules/animals/services/animalsService.js
@@ -0,0 +1,30 @@
+const Animal = require("../models/animal");
+
+const animalRepository = require("../repositories/animalsRepository");
+
+class AnimalsService {
+  constructor(animalRepository) {
+    this.animalRepository = animalRepository;
+  }
+
+  async getAll() {
+    return await this.animalRepository.findAll();
+  }
+
+  async getOneById(id) {
+    return await this.animalRepository.findOneById(id);
+  }
+
+  async create(payload) {
+    const animal = new Animal(payload);
+    return await this.animalRepository.create(animal);
+  }
+
+  updateById(id, payload) {}
+
+  delete(id) {}
+}
+
+const animalsService = new AnimalsService(animalRepository);
+
+module.exports = animalsService;
